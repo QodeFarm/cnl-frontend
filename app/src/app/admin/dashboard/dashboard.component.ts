11a5269d38f080acfd040c64b28201a4c401971d
Chetan0178 <chetan.chaudhari@qodefarm.com>
2024-11-28 10:28:10 +0530

    added pop up model for sales i.e sales-model

--- app/src/app/admin/dashboard/dashboard.component.ts
+++ app/src/app/admin/dashboard/dashboard.component.ts
@@ -9,5 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  isModalOpen: boolean = false;
 
+  // Function to open the modal
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  // Function to close the modal
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
